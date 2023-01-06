c8e63ebef79cb0e2ac483691082db4cb1a4fba80
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2023-01-05 16:56:31 -0800

    add led roulette challenge

--- led_roulette/src/main.rs
+++ led_roulette/src/main.rs
@@ -3,23 +3,30 @@
 #![no_std]
 
 use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
-use volatile::Volatile;
 
 #[entry]
 fn main() -> ! {
     let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    let mut half_period = 500_u16;
+    // let mut half_period = 500_u16;
     // The compiler is smart and recognized that half_period didn't change and instead, in the two
     // places where `delay.delay_ms(half_period);` is called we see `mov.w r1, #500`. So changing
     // half_period when debuging ((gdb) set half_period = ...)
-    let v_half_period = Volatile::new(&mut half_period);
+    // let v_half_period = Volatile::new(&mut half_period);
 
     loop {
-        leds[0].on().ok();
-        delay.delay_ms(v_half_period.read());
-
-        leds[0].off().ok();
-        delay.delay_ms(v_half_period.read());
+        // Turn light on for 150ms, Turn a new light on every 100ms
+        for i in 0..leds.len() {
+            // Turn on an led
+            leds[i].on().ok();
+            // Wait 100ms
+            delay.delay_ms(100u16);
+            // Turn on the next led (if we are at the last led, turn on the first one)
+            leds[if i + 1 > leds.len() - 1 { 0 } else { i + 1 } ].on().ok();
+            // Wait 50ms
+            delay.delay_ms(50u16);
+            // Turn off the first led we turned on
+            leds[i].off().ok();
+        }
     }
 }
