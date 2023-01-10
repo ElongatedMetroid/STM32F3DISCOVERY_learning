7662cabaccba55e569c0c82eebcafd3db97af0ba
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2023-01-09 16:07:30 -0800

    complete registers section

--- registers/src/main.rs
+++ registers/src/main.rs
@@ -1,12 +1,28 @@
 #![no_main]
 #![no_std]
 
+use core::ptr;
+
 #[allow(unused_imports)]
-use aux7::{entry, iprint, iprintln};
+use aux7::{entry, iprint, iprintln, ITM};
+
+/// Print the current contents of ODR
+fn iprint_odr(itm: &mut ITM) {
+    const GPIOE_ODR: u32 = 0x4800_1014;
+
+    unsafe {
+        iprintln!(
+            &mut itm.stim[0],
+            "ODR = 0x{:04x}",
+            // Read bits 0..15 of the ODR register
+            ptr::read_volatile(GPIOE_ODR as *const u16)
+        );
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    aux7::init();
+    let (mut itm, gpioe) = aux7::init();
 
     unsafe {
         // This address points to a register, a register is a special region of memory that controls a peripheral
@@ -60,6 +76,82 @@ fn main() -> ! {
 
         // Turn off the "East" LED
         *(GPIOE_BSRR as *mut u32) = 1 << (11 + 16);
+
+        // This all works when we run it with debug optimizations but it doesnt work with release
+        // optimizations. In short this is because reads/writes to registers are special. We just
+        // wrote four different values to the same register, if you didnt know that address was a
+        // register the logic may have been simpilfied to write just the final value, 1 << (11 + 16)
+        // into the register. LLVM, the compilers backend / optimizer does not know we are dealing
+        // with a register and will merge the writes. To prevent LLVM from misoptimizing our 
+        // program, we can use volatile operations instead of plain reads/writes:
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16));
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
+
+        // Not all the peripheral memory can be accessed, this address is close to the GPIOE_BSSR
+        // address we used before but this address is invalid. Invalid in the sense that there's
+        // no register at this address. 
+        // ptr::read_volatile(0x48001800 as *const u32);
+        // We tried to do an invalid operation, reading memory that doesn't exist, so the processor
+        // raised an exception, a hardware exception. In most cases, exceptions are raised when the
+        // processor attempts to perform an invalid operation. Exceptions break the normal control
+        // flow of a program and force the processor to execute an exception handler, which is just
+        // a function/subroutine. There are different kinds of exceptions. Each kind of exception
+        // is raised by different conditions and each one is handled by a different exception 
+        // handler. The aux7 crate depends on the cortex-m-rt crate which defines a default `hard
+        // fault` handler, named HardFault, that handles the "invalid memory address" exception.
+        
+        // BSRR is not the only register that can control the pins of port E. The ODR register also
+        // lets you change the value of the pins. Furthermore, ODR also lets you retrieve the 
+        // current output status of port E. (ODR is documented on page 239 of the ref. manual)
+        // Print the initial contents of ODR
+        iprint_odr(&mut itm);
+
+        // Turn on the "North" LED (red)
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
+        iprint_odr(&mut itm);
+
+        // Turn on the "East" LED (green)
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
+        iprint_odr(&mut itm);
+
+        // Turn off the "North" LED
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16));
+        iprint_odr(&mut itm);
+
+        // Turn off the "East" LED
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
+        iprint_odr(&mut itm);
+
+        // Type safe manipulation
+        // The last register we were working with, ODR, had this in its documentation: "Bits 32:16
+        // Reserved, must be kept at reset value". We are not supposed to write to those bits of
+        // the register or bad stuff may happen. There's also the fact the registers have different
+        // read/write permissions. Some of them are write only, other can be read and written to
+        // and there must be others that are read only. Finnaly directly working with hexadecimal
+        // addresses is error prone. You already saw that trying to access an invalid memory address
+        // causes an exception which disrupts the execution of our program. It would be a better 
+        // idea to manipulate these registers in a safe manner using an API. Ideally, the API 
+        // should encode these three points mentioned: No messing around with the actual addresses
+        // should respect read/write permissions, and should prevent modification of the reserved
+        // parts of a register. Well there actually is an API for this, the aux7::init() actaully
+        // returns a value that provides a type safe API to manipulate the registers of the GPIOE
+        // peripheral.
+        
+        // As you may remember: a group of registers associated to a peripheral is called a 
+        // register block, and it's located in a contigous region of memory. In this type safe API
+        // each register block is modeled as a struct where each of its fields represents a 
+        // register. Each register field is a different newtype over e.g. `u32` that exposes a 
+        // combination of the following methods, `read`, `write`, or `modify` according to its 
+        // read/write permissions. Finnaly, these methods don't take primitive values like `u32`, 
+        // instead they take yet another newtype that can be constructed using the builder pattern
+        // and that prevent the modification of the reserved parts of the register.
+        // Here is the example of turning on and off the LED's ported to that API
+        gpioe.bsrr.write(|w| w.bs9().set_bit());
+        gpioe.bsrr.write(|w| w.bs11().set_bit());
+        gpioe.bsrr.write(|w| w.br9().set_bit());
+        gpioe.bsrr.write(|w| w.br11().set_bit());
     }
 
     loop {}
